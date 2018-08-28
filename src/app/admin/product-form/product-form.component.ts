810d76787855c05992a016c75f43292cddd982cb
Christian Davel Altan Garrido <CDAltan@Desarrollo.administracion.local>
2018-08-28 17:19:46 -0600

    last commit

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -14,6 +14,7 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
   product = {};
+  id;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,21 +24,29 @@ export class ProductFormComponent implements OnInit {
   ) {
     this.categories$ = catergoryService.getCategories();
 
-    let id= this.route.snapshot.paramMap.get('id');
-    console.log(id);
-    if(id){ 
-      this.productService.get(id).take(1).subscribe(p  => this.product = p );
+    this.id= this.route.snapshot.paramMap.get('id');  
+    if(this.id){ 
+      this.productService.get(this.id).take(1).subscribe(p  => this.product = p );
     }
    }
 
-   save(product){
-      console.log(product);
-      this.productService.create(product);
+   save(product){      
+      if(this.id)  this.productService.update(this.id, product);
+      else this.productService.create(product);
+      
       this.router.navigate(['/admin/products']);
+   }
+
+   delete(){
+     if (!confirm('Esta Seguro de Eliminar el Producto?')) return;
+     
+     this.productService.delete(this.id);
+     this.router.navigate(['/admin/products']);     
    }
 
   ngOnInit() {
   }
+
 
 
 
