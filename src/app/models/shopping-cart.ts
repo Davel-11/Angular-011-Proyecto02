480a8846675cd73915998c94d99897f40cf34fbd
Christian Davel Altan Garrido <CDAltan@Desarrollo.administracion.local>
2018-09-05 09:16:38 -0600

    c4

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,14 +1,16 @@
 import { ShoppingCartItem } from "./shopping-cart-item";
+import { Product } from "./product";
 
 export class ShoppingCart {
+
+    items: ShoppingCartItem[] = [];
         
-    constructor(public items: ShoppingCartItem[]){
-
-    }
-
-    get productIds(){
-      return Object.keys(this.items);
-    }
+    constructor(public itemsMap: { [productId: string]: ShoppingCartItem }  ){
+      for (let productId in itemsMap){
+        let item = itemsMap[productId]
+        this.items.push(new ShoppingCartItem(item.product, item.quantity));
+      }
+    } 
 
     get totalItemsCount(){
         let count = 0;
@@ -18,5 +20,16 @@ export class ShoppingCart {
       return count;
     }
 
+    get totalPrice(){
+      let sum= 0;
+      for (let productId in this.items){
+        sum += this.items[productId].totalPrice;
+      }
+      return sum;
+    }
 
+    getQuantity(product: Product){
+      let item = this.itemsMap[product.$key];
+      return item ? item.quantity: 0 ;  
+    }
 }
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product';
 import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'product-card',
@@ -8,27 +9,15 @@ import { ShoppingCartService } from '../shopping-cart.service';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent  {
-
   @Input('product') product: Product;
   @Input('show-actions') showActions = true; 
-  @Input('shopping-cart') shoppingCart;
+  @Input('shopping-cart') shoppingCart: ShoppingCart;
   
   constructor(private cartService: ShoppingCartService) { }
 
   addToCart(){   
     this.cartService.addToCart(this.product);    
   }
-
-  getQuantity(){
-    if(!this.shoppingCart ) return 0;
-
-    let item = this.shoppingCart.items[this.product.$key];
-    return item ? item.quantity: 0 ;
-
-  }
-
-  removeFromCart(){
-    this.cartService.removeFromCart(this.product);
-  }
+  
 
 }
